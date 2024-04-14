cd41d2e95554188ecb08c0ceb454671680193bf1
chrisspenceratx <chrisspenceratx@gmail.com>
2024-04-14 07:58:46 -0500

    added BackgroundImage

--- components/Screen2.js
+++ components/Screen2.js
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-const Screen2 = () => {
+const Screen2 = ({ route, navigation }) => {
+  const { name } = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
+
  return (
    <View style={styles.container}>
      <Text>Hello Screen2!</Text>
@@ -9,11 +16,11 @@ const Screen2 = () => {
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center'
- }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default Screen2;
