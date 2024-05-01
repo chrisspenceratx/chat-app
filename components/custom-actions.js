0ad8fa634267bbbad170e590076613beb5f417e0
chrisspenceratx <chrisspenceratx@gmail.com>
2024-05-01 06:13:44 -0500

    implement react-native-action-sheet with placeholder functions

--- components/custom-actions.js
+++ components/custom-actions.js
@@ -1,11 +1,37 @@
 import { TouchableOpacity, StyleSheet, View, Text } from "react-native";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 
 const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
-  const onActionPress = () => {}
+  const actionSheet = useActionSheet();
 
+  const onActionPress = () => {
+    const options = ["Choose from Library", "Take Picture", "Send Location", "Cancel"];
+    const cancelButtonIndex = options.length - 1;
+
+    actionSheet.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            console.log("User wants to pick an image");
+            return;
+          case 1:
+            console.log("User wants to take a photo");
+            return;
+          case 2:
+            console.log("User want to get their location");
+          default:
+        }
+      },
+    );
+  };
+  
   return (
     <TouchableOpacity style={styles.container} onPress={onActionPress}>
-    <View style={[styles.wrapper, wrapperStyle]}>
+      <View style={[styles.wrapper, wrapperStyle]}>
         <Text style={[styles.iconText, iconTextStyle]}>+</Text>
       </View>
     </TouchableOpacity>
@@ -13,25 +39,25 @@ const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      width: 26,
-      height: 26,
-      marginLeft: 10,
-      marginBottom: 10,
-    },
-    wrapper: {
-      borderRadius: 13,
-      borderColor: "#b2b2b2",
-      borderWidth: 2,
-      flex: 1,
-    },
-    iconText: {
-      color: "#b2b2b2",
-      fontWeight: "bold",
-      fontSize: 10,
-      backgroundColor: "transparent",
-      textAlign: "center",
-    },
-  });
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: "#b2b2b2",
+    borderWidth: 2,
+    flex: 1,
+  },
+  iconText: {
+    color: "#b2b2b2",
+    fontWeight: "bold",
+    fontSize: 10,
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
+});
 
 export default CustomActions;
